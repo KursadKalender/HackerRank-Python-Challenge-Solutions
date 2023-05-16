f53f5ae2001654f424906069de7108a27f95271f
Muhammed Kürşad Kalender <72703317+KursadKalender@users.noreply.github.com>
2023-05-16 21:16:31 +0300

    Create Set.Symmetric_Difference().py

---
+++ Challenges/Sets/Set.Symmetric_Difference().py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    eng_sub = int(input())
+    eng_numbers = set(map(int, input().split()))
+    fr_sub = int(input())
+    fr_numbers = set(map(int, input().split()))
+    print(len(eng_numbers.symmetric_difference(fr_numbers)))
